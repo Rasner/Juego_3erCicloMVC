acfec8b73e3b9f2c3409e1220f6cf8f858b1789a
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2018-07-06 01:32:55 -0500

    PenduloControlador
    se implemento el controlador de pendulo

--- Juego Mcv/Assets/scripts/Pendulo/Pendulocontrolador.cs
+++ Juego Mcv/Assets/scripts/Pendulo/Pendulocontrolador.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pendulocontrolador : MonoBehaviour {
-
     public static Pendulocontrolador instance;
-	// Use this for initialization
-	public void Start () {
+    // Use this for initialization
+    public void Start() {
         instance = this;
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {
 
-    public void update()
+    }
+
+    public void pendulomov()
     {
         PenduloModelo.instance._startTime += Time.deltaTime;
-        transform.rotation = Quaternion.Lerp(PenduloModelo.instance._start, PenduloModelo.instance._end(Mathf.Sin(PenduloModelo.instance._startTime * PenduloModelo.instance._speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+        transform.rotation = Quaternion.Lerp(PenduloModelo.instance._start, PenduloModelo.instance._end, (Mathf.Sin(PenduloModelo.instance._startTime * PenduloModelo.instance._speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+    }
+
+    public Quaternion PenduloRotation(float angle)
+    {
+        var PenduloRotation = transform.rotation;
+        var angleZ = PenduloRotation.eulerAngles.z + angle;
+
+        if (angleZ > 180)
+            angleZ -= 360;
+        else if (angleZ < -180)
+            angleZ += 360;
+
+        PenduloRotation.eulerAngles = new Vector3(PenduloRotation.eulerAngles.x, PenduloRotation.y, angleZ);
+        return PenduloRotation;
     }
 }
