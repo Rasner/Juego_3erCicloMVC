f3b039db5af476b4a005e93e80b389dcb1eda9e2
Cesar Augusto Navarrete Pacheco <rasner@hotmail.es>
2018-07-06 00:15:12 -0500

    Joystic arreglado
    Denuevo arreglado

--- Juego Mcv/Assets/scripts/Joystick/JoystickModelo.cs
+++ Juego Mcv/Assets/scripts/Joystick/JoystickModelo.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class JoystickModelo : MonoBehaviour, IDragHandler,  IPointerUpHandler, IPointerDownHandler
+public class JoystickModelo : MonoBehaviour
 {
  
     public Image BT, JS;
