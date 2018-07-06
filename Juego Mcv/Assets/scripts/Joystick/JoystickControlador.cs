00a348d445550b743e56b499db36cb39b0c3a7ee
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2018-07-06 00:12:46 -0500

    Joystickcontrolador
    se completo el codigo sin errores

--- Juego Mcv/Assets/scripts/Joystick/JoystickControlador.cs
+++ Juego Mcv/Assets/scripts/Joystick/JoystickControlador.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class JoystickControlador : MonoBehaviour {
+public class JoystickControlador : MonoBehaviour,IDragHandler, IPointerUpHandler,IPointerDownHandler {
 
+    public static JoystickControlador instance;
 	// Use this for initialization
 	void Start () {
-        		
+        instance = this;
 	}
 	
 	// Update is called once per frame
@@ -14,18 +17,35 @@ public class JoystickControlador : MonoBehaviour {
 		
 	}
 
-    public virtual void Ondrag(PointerEventData ped)
+    public virtual void OnDrag(PointerEventData ped)
     {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle
+            (JoystickModelo.instance.BT.rectTransform,
+                ped.position,
+                ped.pressEventCamera,
+                out JoystickModelo.instance.pos))
+        {
+            JoystickModelo.instance.pos.x = (JoystickModelo.instance.pos.x / JoystickModelo.instance.BT.rectTransform.sizeDelta.x);
+            JoystickModelo.instance.pos.y = (JoystickModelo.instance.pos.y / JoystickModelo.instance.BT.rectTransform.sizeDelta.y);
 
+            float x = (JoystickModelo.instance.BT.rectTransform.pivot.x == 1) ? JoystickModelo.instance.pos.x * 2 + 1 : JoystickModelo.instance.pos.x * 2 - 1;
+            float y = (JoystickModelo.instance.BT.rectTransform.pivot.y == 1) ? JoystickModelo.instance.pos.y * 2 + 1 : JoystickModelo.instance.pos.y * 2 - 1;
+
+            JoystickModelo.instance.InputDirection = new Vector3(x, 0, y);
+            JoystickModelo.instance.InputDirection = (JoystickModelo.instance.InputDirection.magnitude > 1) ? JoystickModelo.instance.InputDirection.normalized : JoystickModelo.instance.InputDirection;
+            JoystickModelo.instance.JS.rectTransform.anchoredPosition = new Vector3(JoystickModelo.instance.InputDirection.x * (JoystickModelo.instance.BT.rectTransform.sizeDelta.x / 3)
+                , JoystickModelo.instance.InputDirection.z * (JoystickModelo.instance.BT.rectTransform.sizeDelta.y / 3));
+        }
     }
 
     public virtual void OnPointerDown(PointerEventData ped)
     {
+        JoystickModelo.instance.pos = Vector2.zero;
         OnDrag(ped);
     }
     public virtual void OnPointerUp(PointerEventData ped)
     {
-        InputDirection = Vector3.zero;
-        JS.rectTransform.anchoredPosition = Vector3.zero;
+        JoystickModelo.instance.InputDirection = Vector3.zero;
+        JoystickModelo.instance.JS.rectTransform.anchoredPosition = Vector3.zero;
     }
 }
