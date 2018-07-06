5fa09a0408af3ad7ff3d54817b0ef1297c9ca861
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2018-07-05 23:48:06 -0500

    PlayerControlador
    se implemento el script

--- Juego Mcv/Assets/scripts/Joystick/JoystickControlador.cs
+++ Juego Mcv/Assets/scripts/Joystick/JoystickControlador.cs
@@ -6,11 +6,26 @@ public class JoystickControlador : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public virtual void Ondrag(PointerEventData ped)
+    {
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        InputDirection = Vector3.zero;
+        JS.rectTransform.anchoredPosition = Vector3.zero;
+    }
 }
